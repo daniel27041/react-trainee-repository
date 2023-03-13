0307e8049caf779618746d5f09626f0c642ef57e
Kohajda Dániel <daniel.kohajda@dorsum.eu>
2023-03-13 11:16:57 +0100

    "Életem az ételem..." csináljunk hozzá alkalmazást

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -18,6 +18,20 @@ function App() {
           Learn React
         </a>
       </header>
+
+
+    Készíteni egy alkalmazást, ami egy véletlenszerű ételnek a nevét, az elkészítését és a hozzávalóit megjeleníti a felületen szöveges formában.
+
+    Weboldal:  https://www.themealdb.com/api.php?ref=apilist.fun
+
+    Egy véletlenszerű étel a lekéréséhez URL:
+    https://www.themealdb.com/api/json/v1/1/random.php
+
+    Felhasználandó mezők a válaszból:
+    "strMeal" - étel neve
+    "strInstructions" - elkészítés leírás
+    "strIngredient1",...,"strIngredient20" - mezők a hozzávalókhoz
+    "strMeasure1",...,"strMeasure20" - mezők az egyes hozzávalók mennyiségeihez
     </div>
   );
 }
